25254c4d37c3d8c5e23386c0511ab0ace019b902
이소영 | Lee Soyoung <72489644+gitSoyoungLee@users.noreply.github.com>
2026-09-18 01:07:39 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (90ms, 9MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        for(int i=0; i<5; i++)
+            printStars();
+    }
+
+    static void printStars() {
+        System.out.println("**********");
+    }
+}
