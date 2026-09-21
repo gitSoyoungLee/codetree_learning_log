94c71f18deb4755d1fc98f1eeaeea85e7946f414
이소영 | Lee Soyoung <72489644+gitSoyoungLee@users.noreply.github.com>
2026-09-21 23:44:48 +0900

    [Passed] 재귀함수의 꽃 (132ms, 10MB)

---
+++ trail2/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        print(n);
+    }
+
+    static void print(int n) {
+        if(n==0) {
+            return;
+        }
+        System.out.print(n+" ");
+        print(n-1);
+        System.out.print(n+" ");
+    }
+}
