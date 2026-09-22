d87175d6f7c85880939cfe85633db3add39f275c
이소영 | Lee Soyoung <72489644+gitSoyoungLee@users.noreply.github.com>
2026-09-22 17:07:14 +0900

    [Passed] 재귀함수를 이용한 피보나치 수 (135ms, 10MB)

---
+++ trail2/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        System.out.println(F(n));
+    }
+
+    static int F(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        return F(n - 2) + F(n - 1);
+    }
+}
